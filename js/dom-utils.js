e6dc3ca510890d160c2ee688b749ff4435816b58
kicknback <joshua.borreli@gmail.com>
2021-06-17 18:28:45 -0500

    third card

--- js/dom-utils.js
+++ js/dom-utils.js
@@ -1,27 +1,91 @@
 let domMovieBuilder = movieArr => {
     let main = $("#main");
     main.empty();
+    let movies = [];
     movieArr.forEach(movie => {
         main.append(`
-        <div class="card" data-attribute="${movie.id}">
-          <img class="card-img-top" src="${movie.image}" alt="Card image cap">
+        <div class="card">
+          <img class="card-img-top" src="${movie.image||'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Triforce.svg/1024px-Triforce.svg.png'}" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">${movie.title}</h5>
-            <p class="card-text">${movie.plot}</p>
+            <h5 class="card-title">${movie.title||"(Not found)"}</h5>
+            <p class="card-text">${movie.plot||"(Not found)"}</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">Director: ${movie.director}</li>
-            <li class="list-group-item">Actors: ${movie.actors}</li>
-            <li class="list-group-item">Genre: ${movie.genre}</li>
-            <li class="list-group-item">Year: ${movie.year}</li>
+            <li class="list-group-item">Director: ${movie.director||"(Not found)"}</li>
+            <li class="list-group-item">Actors: ${movie.actors||"(Not found)"}</li>
+            <li class="list-group-item">Genre: ${movie.genre||"(Not found)"}</li>
+            <li class="list-group-item">Year: ${movie.year||"(Not found)"}</li>
           </ul>
-          <!-- <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-          </div> -->
+          <div class="card-body" data-attribute="${movie.id}">
+            <button type="button" class="btn btn-primary rounded edit-movie">Edit Movie</button>
+            <button type="button" class="btn btn-primary rounde delete-movie">Delete</button>
+          </div>
         </div>
     `)
+        movies.push({
+            id: movie.id,
+            image: movie.image,
+            title: movie.title,
+            plot: movie.plot,
+            director: movie.director,
+            actors: movie.actors,
+            genre: movie.genre,
+            year: movie.year
+        });
     })
 
+    $(".edit-movie").click(function () {
+        let currentMovie = $(this).parent().attr("data-attribute");
+        console.log(movies[currentMovie - 2]);
+        formBuilder(movies[currentMovie - 2]);
+    });
+}
+
+
+let formBuilder = formInfo => {
+    let main = $("#main");
+    $(`
+        <h4 class="">Complete change movie form</h4>
+        <form>
+            <div class="form-group">
+                <label for="url">Picture URL</label>
+                <input type="text" class="form-control" id="url" value="${formInfo.image}">
+            </div>
+            <div class="form-group">
+                <label for="Title">Title</label>
+                <input type="text" class="form-control" id="title" value="${formInfo.title}">
+            </div>
+            <div class="form-group">
+                <label for="post">Plot</label>
+                <input type="text" class="form-control" id="plot" value="${formInfo.plot}">
+            </div>
+            <div class="form-group">
+                <label for="director">Director</label>
+                <input type="text" class="form-control" id="director" value="${formInfo.director}">
+            </div>
+            <div class="form-group">
+                <label for="actors">Actors</label>
+                <input type="text" class="form-control" id="actors" value="${formInfo.actors}">
+            </div>
+            <div class="form-group">
+                <label for="genre">Genre</label>
+                <input type="text" class="form-control" id="genre" value="${formInfo.genre}">
+            </div>
+            <div class="form-group">
+                <label for="year">Year</label>
+                <input type="text" class="form-control" id="year" value="${formInfo.year}">
+            </div>
+            
+            <button type="button" class="btn btn-primary" id="changes">Submit changes</button>
+        </form>
+    `).insertAfter(main);
 
 }
+
+
+
+
+
+
+
+
--- js/movie-utils.js
+++ js/movie-utils.js
@@ -1,30 +1,28 @@
-let movieAPICall = () => {
-    fetch("https://stupendous-extreme-slug.glitch.me/movies")
-        .then(res => res.json()).then(data => {
-        console.log(data);
-        let movies = [];
-        data.forEach((movie, index) => {
-            console.log(data[0].actors);
-            movies.push({
-                actors: data[index].actors,
-                director: data[index].director,
-                genre: data[index].genre,
-                id: data[index].id,
-                plot: data[index].plot,
-                image: data[index].poster,
-                rating: data[index].rating,
-                title: data[index].title,
-                year: data[index].year
-            });
-        })
-        domMovieBuilder(movies);
-    }).catch(err => {
-        console.log(`There was an API error of the following: ${err}`);
-        alert(`Sorry, there was an error retrieving movie data.  Please try again later.`)
-    });
-}
-
 $(document).ready(() => {
+    let movies = [];
+    let movieAPICall = () => {
+        fetch("https://stupendous-extreme-slug.glitch.me/movies")
+            .then(res => res.json()).then(data => {
+            // console.log(data);
+            data.forEach((movie, index) => {
+                movies.push({
+                    actors: data[index].actors,
+                    director: data[index].director,
+                    genre: data[index].genre,
+                    id: data[index].id,
+                    plot: data[index].plot,
+                    image: data[index].poster,
+                    rating: data[index].rating,
+                    title: data[index].title,
+                    year: data[index].year
+                });
+            })
+            domMovieBuilder(movies);
+        }).catch(err => {
+            console.log(`There was an API error of the following: ${err}`);
+            alert(`Sorry, there was an error retrieving movie data.  Please try again later.`)
+        });
+    }
     movieAPICall();
 
     $("#add-movie").click(function () {
@@ -48,7 +46,8 @@ $(document).ready(() => {
             console.log(`There was an API error of the following: ${err}`);
             alert(`Sorry, there was an error retrieving movie data.  Please try again later.`)
         });
-    })
+    });
+
 });
 
 
