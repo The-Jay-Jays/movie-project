8576b1f26eda5304b5079ab08625e548a445fd1b
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-06-17 14:47:49 -0500

    finsihed second card

--- js/movie-utils.js
+++ js/movie-utils.js
@@ -1,5 +1,3 @@
-
-
 let movieAPICall = () => {
     fetch("https://stupendous-extreme-slug.glitch.me/movies")
         .then(res => res.json()).then(data => {
@@ -28,7 +26,32 @@ let movieAPICall = () => {
 
 $(document).ready(() => {
     movieAPICall();
+
+    $("#add-movie").click(function () {
+        let title = $("#Title").val();
+        let rating = $("#Rating").val();
+        let addMovie = {
+            title: title,
+            rating: rating
+        }
+        fetch("https://stupendous-extreme-slug.glitch.me/movies",{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(addMovie)
+        })
+            .then(res => res.json()).then(data => {
+            console.log(data);
+
+        }).catch(err => {
+            console.log(`There was an API error of the following: ${err}`);
+            alert(`Sorry, there was an error retrieving movie data.  Please try again later.`)
+        });
+    })
 });
+
+
 
 
 
